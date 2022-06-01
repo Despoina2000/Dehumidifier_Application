82b42286fdb11bfd7427712b558bcfcce25b4723
ChristosRousas <christosrousas@gmail.com>
2022-06-01 14:07:18 +0300

    2nd cycle Completed Prototype and 3rd cycle Menu and modes

---
+++ ThirdCycle/DehumidifierApp/modes.js
@@ -0,0 +1,18 @@
+if(document.getElementById("EcoMode").checked) {
+    chosen_hum = "hum1";
+    chosen_fan = "fan1";
+}else if(document.getElementById("DryClothes").checked) {
+    chosen_hum = "hum2";
+    chosen_fan = "fan2";
+}else if(document.getElementById("Auto").checked) {
+    chosen_hum = "hum3";
+    chosen_fan = "fan3";
+}
+var h = document.getElementById(chosen_hum);
+var f = document.getElementById(chosen_fan);
+var humidity = h.value;
+var fanspeed = f.value;
+window.alert(humidity)
+
+document.getElementById("humidityout").innerHTML = humidity;
+document.getElementById("fanspeedout").innerHTML = fanspeed;
